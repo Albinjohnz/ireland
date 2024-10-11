5eb3c370633db93ec3f94dee699d567c4d3be6cc
Albinjohnz <86572237+Albinjohnz@users.noreply.github.com>
2024-10-11 11:17:35 +0100

    basket final push

---
+++ basket.js
@@ -0,0 +1,12 @@
+let basket=(a,b) => {
+    let total=0;
+    for(let fruits in a){
+        total+= a[fruits] * b[fruits];
+        
+    }
+return total;
+}
+a={papaya:10, peach:2};
+b={papaya:5,peach:4};
+let total=basket(a,b);
+alert(total);
