46c601f65d56bdc75f38ca027763cd816bb402f0
Albinjohnz <86572237+Albinjohnz@users.noreply.github.com>
2024-10-21 08:09:55 +0100

    push

--- basket.js
+++ basket.js
@@ -1,13 +1,28 @@
-alert("basket.js")
-let basket=(a,b) => {
-    let total=0;
-    for(let fruits in a){
-        total+= a[fruits] * b[fruits];
-        
+let basket = (quantity, price) => {
+    let cost = 0;
+    for (let item in quantity) {
+        if (price[item] !== undefined) {
+            cost += quantity[item] * price[item];
+        }
     }
-return total;
-}
-a={papaya:10, peach:2};
-b={papaya:5,peach:4};
-let total=basket(a,b);
-alert("total cost of basket is ="+total);
+    return cost;
+};
+
+let quantities = {};
+let prices = {};
+
+let userInput = () => {
+    let appleQuantity = parseFloat(prompt("Enter the quantity of apples:"));
+    let applePrice = parseFloat(prompt("Enter the price of apples:"));
+    quantities["apples"] = appleQuantity;
+    prices["apples"] = applePrice;
+
+    let orangeQuantity = parseFloat(prompt("Enter the quantity of oranges:"));
+    let orangePrice = parseFloat(prompt("Enter the price of oranges:"));
+    quantities["oranges"] = orangeQuantity;
+    prices["oranges"] = orangePrice;
+
+    alert("Total is: " + basket(quantities, prices));
+};
+
+userInput();
